e9346bef72613b3e56d789346dee44ed1511d844
Dmitry Ulupov <dmitry.ulupov@thecut.net.au>
2015-01-27 15:25:08 +0800

    modified import in widgets for django 1.6 (util was renamed to utils in 1.7)

--- thecut/stripe/widgets.py
+++ thecut/stripe/widgets.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django import forms
+try:
+    from django.forms.utils import flatatt
+except ImportError:
+    from django.forms.util import flatatt
 from django.utils.encoding import force_text
 from django.utils.html import format_html
 
@@ -15,4 +19,4 @@ class NamelessTextInput(forms.TextInput):
         if value != '':
             # Only add the 'value' attribute if a value is non-empty.
             final_attrs['value'] = force_text(self._format_value(value))
-        return format_html('<input{0} />', forms.utils.flatatt(final_attrs))
+        return format_html('<input{0} />', flatatt(final_attrs))
