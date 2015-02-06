88e9633158c764a34a220e2e24c2de6645411ff6
Dmitry Ulupov <dmitry.ulupov@thecut.net.au>
2015-02-06 15:23:23 +0800

    modified Charge.create to work with or without customer

--- thecut/stripe/querysets.py
+++ thecut/stripe/querysets.py
@@ -6,8 +6,34 @@ from django.db import models
 class KnownFieldMixin(object):
 
     def _get_known_field_value(self, field_name):
+        """
+        If queryset was called on concrete model (like account.charges.all())
+        this method can be used to get this concrete model.
+
+
+        Note: It returns None if it can't find field (instead of raising
+            KeyError).
+        Example:
+        if queryset is called like this:
+            ``account123.charges.create(...)``
+        inside ``.create`` method call to
+            ``self._get_known_field_value('account')``
+        will return ``account123``.
+
+        But inside ``Charge.objects.create(..)`` it will return ``None``.
+
+        Note 2: if the child model (``charge`` in the example) does not have
+            ``field_name`` field this method will raise ``FieldDoesNotExist``.
+
+        """
         field = self.model._meta.get_field_by_name(field_name)[0]
-        return self._known_related_objects[field].values()[0]
+        try:
+            return self._known_related_objects[field].values()[0]
+        except KeyError:
+            return None
+
+    def _get_account(self):
+        return self._get_known_field_value('account')
 
 
 class ChargeQuerySet(KnownFieldMixin, models.query.QuerySet):
@@ -15,21 +41,63 @@ class ChargeQuerySet(KnownFieldMixin, models.query.QuerySet):
     def _get_customer(self):
         return self._get_known_field_value('customer')
 
-    def create(self, customer=None, **kwargs):
-        customer = customer or self._get_customer()
-        # Create a charge using the stripe API
-        stripe_charge = customer.api().charges().create(
-            customer=customer.stripe_id, **kwargs)
-        # Create a charge model instance
-        return super(ChargeQuerySet, self).create(
-            stripe_id=stripe_charge.id, account=customer.account,
-            customer=customer)
+    def create(self, customer=None, account=None, **kwargs):
+        """
+        Charges can be created in various different ways:
+
+        1) Charge.objects.create(account=aa, amount=xx, card=yy)
+            - no customer set, charge on a connected account
+            - account has to be provided
+        2) account.charges.create(amount=xx, card=yy)
+        3) customer.charges.create(amount=xx[, card=yy])
+            - Resulting charge will have customer set
+            - account is optional
+
+        This method does sanity checks for parameters and raises ``ValueError``
+        if combination of customer and account parameters and call method does
+        not make sense.
+
+        :return: :py:class:`thecut.stripe.models.Charge`
+        """
+
+        # "parent" as in before .
+        # If we are called on concrete model like this ``acc.charges.create()``
+        # parent_account = acc
+        parent_customer = self._get_customer()
+        parent_account = self._get_account()
+
+        if customer and parent_customer and customer != parent_customer:
+            # If called ``customer_john.charges.create(customer=customer_dick)``
+            raise ValueError("Conflicting customer instances provided")
+        if account and parent_account and account != parent_account:
+            # If called ``account_a.charges.create(account=account_b)``
+            raise ValueError("Conflicting account instances provided")
+
+        customer = customer or parent_customer
+        account = account or parent_account
+        if customer and account and customer.account != account:
+            raise ValueError("Customer has conflicting account")
+
+        if customer:
+            # Create a charge using the stripe's customer api
+            stripe_charge = customer.api().charges().create(
+                customer=customer.stripe_id, **kwargs)
+            # Create a charge model instance
+            return super(ChargeQuerySet, self).create(
+                stripe_id=stripe_charge.id, account=customer.account,
+                customer=customer)
+        elif account:
+            # Create a charge using the stripe API
+            stripe_charge = self.model._stripe.Charge.create(
+                api_key=account.secret_key, **kwargs)
+            # Create a customer model instance
+            return super(ChargeQuerySet, self).create(
+                stripe_id=stripe_charge.id, account=account)
+        else:
+            raise ValueError("Account or customer should be provided")
 
 
 class CustomerQuerySet(KnownFieldMixin, models.query.QuerySet):
-
-    def _get_account(self):
-        return self._get_known_field_value('account')
 
     def create(self, account=None, plan=None, **kwargs):
         account = account or self._get_account()
@@ -48,9 +116,6 @@ class CustomerQuerySet(KnownFieldMixin, models.query.QuerySet):
 
 
 class PlanQuerySet(KnownFieldMixin, models.query.QuerySet):
-
-    def _get_account(self):
-        return self._get_known_field_value('account')
 
     def create(self, account=None, **kwargs):
         account = account or self._get_account()
